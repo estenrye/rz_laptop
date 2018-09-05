861bb98f5417d7e99fd6363cb9e280d50386bff4
Esten Rye <esten.rye@ryezone.com>
2018-09-04 19:31:05 -0700

    rename cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name 'rz_laptop_setup'
+name 'rz_laptop'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
-description 'Installs/Configures rz_laptop_setup'
-long_description 'Installs/Configures rz_laptop_setup'
+description 'Installs/Configures rz_laptop'
+long_description 'Installs/Configures rz_laptop'
 version '0.1.0'
 chef_version '>= 14' if respond_to?(:chef_version)
 
@@ -11,12 +11,12 @@ chef_version '>= 14' if respond_to?(:chef_version)
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/rz_laptop_setup/issues'
+# issues_url 'https://github.com/<insert_org_here>/rz_laptop/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/rz_laptop_setup'
+# source_url 'https://github.com/<insert_org_here>/rz_laptop'
 
 cookbook 'timezone_iii'
